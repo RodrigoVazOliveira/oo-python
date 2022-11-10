d52f06327415b06992ceeac81e6ab02cda6908ae
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-11-10 00:45:32 -0300

    Adicionar conversão de moedas

--- extractor_url/get_url.py
+++ extractor_url/get_url.py
@@ -11,8 +11,19 @@ coin_origin_value = extractor_url.get_value_parameter('moedaOrigem')
 coin_destination_value = extractor_url.get_value_parameter('moedaDestino')
 quantity = extractor_url.get_value_parameter('quantidade')
 
+VALUE_DOLAR = 5.59
+
+if coin_origin_value == "real" and coin_destination_value == "dolar":
+    print(f'O valor convertido de R$ {quantity} reais para $ {int(quantity) * VALUE_DOLAR} dólares')
+elif coin_origin_value == "dolar" and coin_destination_value == "real":
+    print(f'O valor convertido de $ {quantity} dólares para  R$ {int(quantity) / VALUE_DOLAR} reais')
+else:
+    print('Câmbio não suportado!')
+
 print("url base: {}, parametros: {}".format(url_base, url_parameters))
 print("valorOrigin: {}, valorDestino: {}, quantidade: {}".format(coin_origin_value, coin_destination_value, quantity))
+
+
 
 
 print(f'A quantidade de caracteres da URL é de {len(extractor_url)}')
